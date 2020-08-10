0bb8dad4f4cd6679855130b06a4cd0cbf4c7aacf
adrianctwo <adrian.e.situ@gmail.com>
2020-08-10 16:11:35 -0700

    update header layout and design

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-<div>
-  Header
+<div className="Header">
+  <div className="HeaderGroup">
+    <Link to="/"><img src={require('../images/personalLogo.svg')} width="30" /></Link>
+    <Link to="/about">About</Link>
+    <Link to="/service">Service</Link>
+    <Link to="/buy"><button>Buy</button></Link>
+    <Link to="/contact">Contact</Link>
+  </div>
 </div>
 )
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Code and Design for Web Development</h1>
+        <h1>Code and Design<br /> for Web Development</h1>
         <p>Hi, I'm Adrian. Nice to meet you! I'm very passionate about using technology to make lives easier. Natural strength in creativity and an eye for design.</p>
         <Link to="/page-2/">Go to page 2</Link>
       </div>
