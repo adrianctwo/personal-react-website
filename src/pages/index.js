ce9729a11317104039006adb2b93da7552915f99
adrianctwo <adrian.e.situ@gmail.com>
2020-08-10 14:55:45 -0700

    Landing page update with some website description

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Code and Design for Web Development</h1>
-    <p>Hi, I'm Adrian. Nice to meet you! I'm very passionate about using technology to make lives easier. Natural strength in creativity and an eye for design. Graduated from Web Development Certificate: University of Washington, Seattle, WA. A program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node JS, MySQL, MongoDB, Express, Handelbars.js & React.JS
-    </p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Code and Design for Web Development</h1>
+        <p>Hi, I'm Adrian. Nice to meet you! I'm very passionate about using technology to make lives easier. Natural strength in creativity and an eye for design.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
