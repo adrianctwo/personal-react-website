7f71d3297d03c2a95c8cd3914d4e8d4cebe2b1cf
adrianctwo <adrian.e.situ@gmail.com>
2020-08-09 22:31:36 -0700

    inital commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Adrian Situ Design Code',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Code and Design for Web Development</h1>
+    <p>Hi, I'm Adrian. Nice to meet you. I'm very passionate about using technology to make lives easier. Natural strength in creativity and an eye for design. Graduated from Web Development Certificate: University of Washington, Seattle, WA. A program focused on gaining technical programming skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node JS, MySQL, MongoDB, Express, Handelbars.js & React.JS
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
