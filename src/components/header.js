6057f765b0bcb85b006d8ba0ff58dcc00099feb5
adrianctwo <adrian.e.situ@gmail.com>
2020-08-11 11:51:52 -0700

    Header now has a state if scrolled, background changes to black

--- src/components/header.js
+++ src/components/header.js
@@ -2,15 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-<div className="Header">
-  <div className="HeaderGroup">
-    <Link to="/"><img src={require('../images/personalLogo.svg')} width="30" /></Link>
-    <Link to="/about">About</Link>
-    <Link to="/service">Service</Link>
-    <Link to="/buy"><button>Buy</button></Link>
-    <Link to="/contact">Contact</Link>
-  </div>
-</div>
-)
-export default Header
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.HandleScroll)
+  }
+
+  HandleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if(scrollTop > 50) {
+      this.setState({hasScrolled: true})
+    } else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/personalLogo.svg')} width="30" /></Link>
+          <Link to="/about">About</Link>
+          <Link to="/service">Service</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    )
+  }
+}
+  export default Header
